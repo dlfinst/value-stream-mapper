d0e45061b3574ac59593d41e7d71033b820d6900
bdfinst <bdfinst@gmail.com>
2018-12-29 08:35:18 -0600

    Parse YAML from Swagger generator

--- api-v1/api-doc.js
+++ api-v1/api-doc.js
@@ -1,1 +1,16 @@
-module.exports = require('./swagger.json')
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const logger = require('../utils').logger('api-doc');
+
+let apiDoc
+
+try {
+  apiDoc = yaml.safeLoad(fs.readFileSync(path.resolve(__dirname, './swagger.yaml'), 'utf8'));
+  logger.msg(apiDoc.paths);
+} catch (e) {
+  logger.err(e);
+  apiDoc = {}
+}
+
+module.exports = apiDoc
