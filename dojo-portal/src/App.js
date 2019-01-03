450a013a4d4119bf8dbefc91364b0432a4adc6d3
Dana Finster <dlfinst@users.noreply.github.com>
2019-01-02 19:28:48 -0600

    added dojo-portal ui and moved value-stream-service to separate folder (#1)
    * added dojo-portal ui and moved value-stream-service to separate folder
    
    * rebrand to virtual dojo

---
+++ dojo-portal/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logo from './logo.png';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to the Virtual Dojo!
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
